ca74bf70049fa5805629620008b22aee8530dd57
rbzf <anna.sadkowska@gmail.com>
2023-04-18 07:27:28 +0200

    Rozwiazanie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,46 +9,49 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         File file = new File("employees.csv");
-        file.createNewFile();
 
         if (file.exists()) {
             File stats = new File("stats.txt");
-            stats.createNewFile();
 
             int employeesNumber = getFileLineNumber(file);
             Employee[] employees = createEmployeesArray(file, employeesNumber);
 
             FileWriter fileWriter = new FileWriter(stats);
             fileWriter.write("Średnia wypłata: " + Statistics.avgSalary(employees) + "\n");
-            fileWriter.write("Najnizsza wypłata: " + Statistics.minSalary(employees) + "\n");
-            fileWriter.write("Najwyzsza wypłata: " + Statistics.maxSalary(employees) + "\n");
-            fileWriter.write("Liczba pracownikow w dziale IT: " + Statistics.numberEmployeesInDep(employees, "IT") + "\n");
-            fileWriter.write("Liczba pracownikow w dziale Management: " + Statistics.numberEmployeesInDep(employees, "Management") + "\n");
-            fileWriter.write("Liczba pracownikow w dziale Support: " + Statistics.numberEmployeesInDep(employees, "Support") + "\n");
+            fileWriter.write("Minimalna wypłata: " + Statistics.minSalary(employees) + "\n");
+            fileWriter.write("Maksymalna wypłata: " + Statistics.maxSalary(employees) + "\n");
+            fileWriter.write("Liczba pracowników IT: " + Statistics.numberEmployeesInDep(employees, "IT") + "\n");
+            fileWriter.write("Liczba pracowników Management: " + Statistics.numberEmployeesInDep(employees, "Management") + "\n");
+            fileWriter.write("Liczba pracowników Support: " + Statistics.numberEmployeesInDep(employees, "Support") + "\n");
 
             fileWriter.close();
         }
 
     }
 
-    private static Employee[] createEmployeesArray(File file, int employeesNumber) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
+    private static Employee[] createEmployeesArray(File file, int employeesNumber) {
         Employee[] employees = new Employee[employeesNumber];
-        for (int i = 0; i < employeesNumber; i++) {
-            String[] line = scanner.nextLine().split(";");
-            employees[i] = new Employee(line[0], line[1], line[2], line[3], Double.parseDouble(line[4]));
+        try (Scanner scanner = new Scanner(file)) {
+            for (int i = 0; i < employeesNumber; i++) {
+                String[] line = scanner.nextLine().split(";");
+                employees[i] = new Employee(line[0], line[1], line[2], line[3], Double.parseDouble(line[4]));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku");
         }
         return employees;
     }
 
-    private static int getFileLineNumber(File file) throws FileNotFoundException {
+    private static int getFileLineNumber(File file) {
         int lines = 0;
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            scanner.nextLine();
-            lines++;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                lines++;
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku");
         }
-        scanner.close();
         return lines;
     }
 }
